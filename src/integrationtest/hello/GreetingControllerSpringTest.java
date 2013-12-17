756b98fec651b7ce8f56250d558c6277dd346fe5
Alex Bepple <alex@bepple.de>
2013-12-17 12:17:49 +0100

    + add back in intermediate Spring integration test
    The test makes sure that we provide a `defaultvalue` in `@RequestParam`.

---
+++ src/integrationtest/hello/GreetingControllerSpringTest.java
@@ -0,0 +1,40 @@
+package hello;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Matchers.notNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+public class GreetingControllerSpringTest {
+
+	@InjectMocks
+	private GreetingController greetingController;
+
+	@Mock
+	private GreetingService greetingService;
+
+	@Mock
+	private DictatorDetector __detector;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		initMocks(this);
+		mockMvc = MockMvcBuilders.standaloneSetup(greetingController).build();
+	}
+
+	@Test
+	public void providesDefaultForName() throws Exception {
+		mockMvc.perform(get("/greet"));
+		verify(greetingService).greet(notNull(String.class));
+	}
+
+}
